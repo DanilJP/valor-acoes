770717205808f0576bbde4ea31bc54e9b95d3003
Daniel Juliano <bi03@GRUPOALMARIA.LOCAL>
2022-05-26 23:29:58 -0300

    versão 2

--- main.py
+++ main.py
@@ -3,20 +3,22 @@ from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
 import pandas as pd
 
-empresas = ['vale','petrobras','banco inter','itausa','bradesco','btg','wege','yduq']
+empresas = ['btg','safra','itau','bradesco','santander','microsoft','facebook','inter','amazon','daniel','petrobras']
 
 
 navegador = webdriver.Chrome()
 infos = {"Empresa": [], "Valor" : []}
 for empresa in empresas:
     navegador.get("https://www.google.com/search?q=acoes+{}".format(empresa))
-    navegador.find_element(by=By.CLASS_NAME, value='aviV4d')
-    page_content = navegador.page_source
-    site = BeautifulSoup(page_content, 'html.parser')
-    rastreios = site.findAll('span', attrs={'jsname': "vWLAgc"})
-    x = rastreios[0]
-    infos['Empresa'].append(empresa)
-    infos['Valor'].append(x.getText())
-
+    try:
+        navegador.find_element(by=By.CLASS_NAME, value='aviV4d')
+        page_content = navegador.page_source
+        site = BeautifulSoup(page_content, 'html.parser')
+        rastreios = site.findAll('span', attrs={'jsname': "vWLAgc"})
+        x = rastreios[0]
+        infos['Empresa'].append(empresa)
+        infos['Valor'].append(x.getText())
+    except:
+        pass
 print(pd.DataFrame(infos))
 navegador.quit()
